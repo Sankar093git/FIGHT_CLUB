4aa41cf1a47fb799e24648b2cb563284d6e3ff13
Sankar093git <sreesankar093@gmail.com>
2025-12-14 04:50:30 +0530

    Cleared errors and bugs in brand page admin's side

--- models/brandSchema.js
+++ models/brandSchema.js
@@ -12,6 +12,10 @@ const brandSchema= new mongoose.Schema({
     isUnlisted:{
         type:Boolean,
         default:false
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false
     }
 
 },{timestamps:true});
--- routers/admin/adminRoutes.js
+++ routers/admin/adminRoutes.js
@@ -40,6 +40,8 @@ router.get("/edit-product",adminAuth,productController.loadEditProduct);
 router.post("/edit-product/:id",adminAuth,uploads.array("images",4),productController.editproduct);
 router.post("/delete-image/:id",adminAuth,productController.deleteImages);
 router.post("/block-or-unblock-products/:id",adminAuth,productController.blockOrUnblockproduct);
+router.delete("/delete-brand/:id",adminAuth,brandController.deleteBrand)
+router.patch("/block-or-unblock/:id",adminAuth,brandController.blockORunblockBrand)
 
 //Brand management
 router.get("/brands",adminAuth,brandController.getBrandList);
